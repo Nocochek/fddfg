69c9c7984d5b7d605078e63b47c3c28e948db308
Nocochek <135050454+Nocochek@users.noreply.github.com>
2023-12-16 13:39:05 +0300

    Add files via upload

---
+++ js/header-munu.js
@@ -0,0 +1,7 @@
+const menu = document.querySelector('.header-nav')
+const burger = document.querySelector('#burger')
+
+burger.addEventListener('click',function(){
+    menu.classList.toggle('desctop')
+    
+})
